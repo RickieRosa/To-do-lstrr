582be49d40c5356f4cf8657b5a54e4991de041e2
RickieRosa <rckrosa@gmail.com>
2021-09-12 03:37:48 -0300

    Santander Bootcamp | Mobile Developer

--- app/src/main/java/br/com/chaos/to_do_lstrr/MainActivity.kt
+++ app/src/main/java/br/com/chaos/to_do_lstrr/MainActivity.kt
@@ -1,11 +1,26 @@
 package br.com.chaos.to_do_lstrr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.chaos.to_do_lstrr.databinding.ActivityMainBinding
+import br.com.chaos.to_do_lstrr.ui.AddTaskActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+            binding.fab.setOnClickListener {
+                startActivity(Intent(this, AddTaskActivity::class.java))
+            }
     }
 }
